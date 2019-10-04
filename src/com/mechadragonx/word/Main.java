3c7fb7a4b09901eae313c61f42ce53e1fd35745e
MechaDragonX <sonicraghav@gmail.com>
2019-10-03 17:39:00 -0700

    Implemented new Breadth-first Search Method AND IT WORKSgit status

--- src/com/mechadragonx/word/Main.java
+++ src/com/mechadragonx/word/Main.java
@@ -2,9 +2,8 @@ package com.mechadragonx.word;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Main
@@ -13,8 +12,10 @@ public class Main
 
     public static void main(String[] args) throws FileNotFoundException
     {
-        readDictionary(".\\data\\dictionary_words_small.txt");
-        print("dog", "cat", Lev("dog", "cat"));
+        readDictionary(".\\data\\dang-tent.txt");
+        String start = "dang";
+        String target = "tent";
+        print(start, target, vlad(start, target));
     }
     private static void readDictionary(String path) throws FileNotFoundException
     {
@@ -25,6 +26,162 @@ public class Main
             while(scanner.hasNextLine())
             {
                 words.add(scanner.nextLine());
+            }
+        }
+    }
+    private static int vlad(String start, String target)
+    {
+        if(start.length() != target.length())
+        {
+            return -1;
+        }
+        if(!words.contains(start) || !words.contains(target))
+        {
+            return -1;
+        }
+
+        int differences = 0;
+        HashSet<String> previous;
+        HashSet<String> current = new HashSet<>();
+        int editCount = 0;
+
+        previous = findDistance1(start, differences);
+        previous.add(start);
+        editCount++;
+
+        while(true)
+        {
+            previous.addAll(current);
+            current.clear();
+            differences = 0;
+            for(String value : previous)
+            {
+                current.addAll(findDistance1(value, differences));
+            }
+            current.removeAll(previous);
+            editCount++;
+            if(current.isEmpty())
+            {
+                return -1;
+            }
+            for(String node : current)
+            {
+                if(node.equals(target))
+                {
+                    return editCount;
+                }
+            }
+        }
+    }
+    private static HashSet<String> findDistance1(String node, int differences)
+    {
+        HashSet<String> set = new HashSet<>();
+        for(String word : words)
+        {
+            if(word.length() == node.length())
+            {
+                for(int i = 0; i < node.length(); i++)
+                {
+                    if(node.charAt(i) != word.charAt(i))
+                    {
+                        differences++;
+                    }
+                }
+                if(differences == 1)
+                {
+                    set.add(word);
+                }
+            }
+            differences = 0;
+        }
+        return set;
+    }
+
+    private static int newLev(String start, String target)
+    {
+        if(start.length() != target.length())
+        {
+            return 0;
+        }
+        if(!words.contains(target) && !words.contains(start))
+        {
+            return 0;
+        }
+
+        TreeSet<String> distance1 = new TreeSet<>(); // All words 1 edit distance away from start
+        String one = start; // Word to compare
+        int editCount = 0;
+        int differences = 0; // Differences between start and word in dictionary
+        int occurences = 0; // Amount of times letters in target are present in distance1
+        int k = 0; // Index variable when looping through distance1 a second time
+        String next = ""; // Next word to compare
+
+        while(true)
+        {
+            for(String word : words)
+            {
+                if(word.length() == one.length())
+                {
+                    for(int i = 0; i < one.length(); i++)
+                    {
+                        if(one.charAt(i) != word.charAt(i))
+                        {
+                            differences++;
+                        }
+                    }
+                    if(differences == 1)
+                    {
+                        distance1.add(word);
+                    }
+                }
+                differences = 0;
+            }
+
+            for(String string : distance1)
+            {
+                for(int j = 0; j < string.length(); j++)
+                {
+                    if (string.charAt(j) == target.charAt(j))
+                    {
+                        ++occurences;
+                    }
+                }
+            }
+            if(occurences == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                for(String value : distance1)
+                {
+                    for(char letter : value.toCharArray())
+                    {
+                        if((letter == target.charAt(k)) && (one.charAt(k) != letter))
+                        {
+                            next = value;
+                            editCount++;
+                            break;
+                        }
+                        k++;
+                    }
+                    if(!next.isEmpty())
+                    {
+                        break;
+                    }
+                    k = 0;
+                }
+            }
+
+            if(next.equals(target))
+            {
+                return editCount;
+            }
+            else
+            {
+                distance1.clear();
+                one = next;
+                next = "";
             }
         }
     }
@@ -66,7 +223,7 @@ public class Main
                         editCount++;
                         return editCount;
                     }
-                    else if(test.charAt(i) == target.charAt(i))
+                    else if((test.charAt(i) == target.charAt(i)) && words.contains(test))
                     {
                         newLetter = 'a';
                         editCount++;
@@ -107,9 +264,13 @@ public class Main
     }
     private static void print(String start, String target, int editCount)
     {
-        if(editCount == 0)
+        if(editCount == -1)
         {
             System.out.println("No path found!");
+        }
+        else if(editCount == 0)
+        {
+            System.out.println("They are the same word.");
         }
         else
         {
